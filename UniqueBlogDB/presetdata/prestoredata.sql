9b696c76cb79721302e0a448bb00cbd297317c47
dieshangtianya <woheaven@sina.com>
2016-11-04 17:32:55 +0800

    change db to support chinese

--- UniqueBlogDB/presetdata/prestoredata.sql
+++ UniqueBlogDB/presetdata/prestoredata.sql
@@ -1,4 +1,15 @@
-﻿use UniqueBlogDB;
+﻿/*===================================
+Tips:
+There are three ways to avoid the messy code in SQL SERVER database.
+
+(1) Create database with the right collation.
+(2) Specify the collation on the database.
+(3) If we use the SQL SERVER database which uses the english platform, if we insert chinese, it will show with mojibake. 
+	so here we can insert values prefix with 'N'. First of all we should change the db table column type to nvarchar which column potentially contains chinese.
+
+=====================================*/
+
+use UniqueBlogDB;
 
 --t_user
 INSERT INTO t_user (UserName,Email,[Password],NickName) values('frwang','woheaven@sina.com','3748511','蝶殇天涯');
