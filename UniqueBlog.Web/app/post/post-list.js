8398aa38638cc8da1da90b853686fc12035aaa55
freddywang <woheaven@sina.com>
2016-12-30 18:10:54 +0800

    1. change the blog layout and theme color to black. 2. change the calendar theme to meet the theme. 3. change the content style to make it more elegant

--- UniqueBlog.Web/app/post/post-list.js
+++ UniqueBlog.Web/app/post/post-list.js
@@ -4,5 +4,7 @@
 ], function ($, domready, dotdotdot) {
     domready(function () {
         $(".post-item-brief").dotdotdot({ watch: "window" });
+
+        $(".post-item-brief").addClass("post-item-visible");
     })
 })
