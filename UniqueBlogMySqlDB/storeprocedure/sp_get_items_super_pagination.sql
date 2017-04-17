ce82cf2d2308b1ec543e9535b07e5efd3ff7e56e
dieshangtianya <woheaven@sina.com>
2017-04-17 23:41:47 +0800

    change pagination procedure

--- UniqueBlogMySqlDB/storeprocedure/sp_get_items_super_pagination.sql
+++ UniqueBlogMySqlDB/storeprocedure/sp_get_items_super_pagination.sql
@@ -1,26 +1,23 @@
-﻿use UniqueBlogDB;
-
-DROP PROCEDURE IF EXISTS sp_get_items_super_pagination;
-
-CREATE PROCEDURE sp_get_items_super_pagination
-(
+﻿drop PROCEDURE if EXISTS sp_get_items_super_pagination;
+create procedure sp_get_items_super_pagination(
 IN TableName VARCHAR(5000),
 IN `Fields` VARCHAR(5000),
 IN SqlWhere VARCHAR(5000),  
 IN GroupFields VARCHAR(5000),
 IN OrderByFields VARCHAR(5000),
 IN PageIndex INT,
-IN PageSize INT, 
+IN PageSize INT,
 OUT TotalRecordAmount INT
 )
 BEGIN
 
 /**declare two parameter to store the total page amount and sql which will be executed later**/
-DECLARE TotalPageAmount INT;
-DECLARE `Sql` TEXT;
+DECLARE TotalPageAmount INT DEFAULT 0;
 
-DECLARE StartRecord INT;
-DECLARE EndRecord INT;
+DECLARE StartRecord INT DEFAULT 0;
+DECLARE EndRecord INT DEFAULT 0;
+
+SET @SqlCommand='';
 
 IF SqlWhere IS NOT NULL AND LENGTH(SqlWhere)>0 AND LOCATE('WHERE',UPPER(LTRIM(SqlWhere)))!=1 THEN
 	SET SqlWhere = CONCAT(' WHERE ',SqlWhere);
@@ -37,24 +34,31 @@ END IF;
 IF SqlWhere IS NULL OR SqlWhere='' THEN
 	IF GroupFields IS NULL OR GroupFields='' THEN
 		/*if SqlWhere and GroupFields are both null or empty*/
-		SET `Sql`=CONCAT('SELECT TotalRecordAmount = COUNT(*) FROM ' , TableName);
+		SET @SqlCommand=CONCAT('SELECT @TotalRecords:= COUNT(*) FROM ' , TableName);
 	ELSE
 		/*if SqlWhere is null but GroupFields is not null*/
-		SET `Sql`=CONCAT('SELECT TotalRecordAmount = COUNT(*) FROM (SELECT ', `Fields`,' FROM ' , TableName , ' ' , GroupFields, ') AS tempTb');
+		SET @SqlCommand=CONCAT('SELECT @TotalRecords:= COUNT(*) FROM (SELECT ', `Fields`,' FROM ' , TableName , ' ' , GroupFields, ') AS tempTb');
 	END IF;
 ELSE
 	IF GroupFields IS NULL OR GroupFields='' THEN
 		/*if SqlWhere is not null or empty, but the GroupFields is null or empty*/
-		SET `Sql`=CONCAT('SELECT TotalRecordAmount = COUNT(*) FROM ' , TableName , ' ' , SqlWhere);
+		SET @SqlCommand=CONCAT('SELECT @TotalRecords:= COUNT(*) FROM ' , TableName , ' ' , SqlWhere);
 	ELSE
 		/*if SqlWhere and GroupFields both are not null or empty*/
-		SET `Sql`=CONCAT('SELECT TotalRecordAmount = COUNT(*) FROM (SELECT ', `Fields` , ' FROM ' , TableName , ' ' , SqlWhere , ' ',GroupFields , ') AS tempTb');
+		SET @SqlCommand=CONCAT('SELECT @TotalRecords:= COUNT(*) FROM (SELECT ', `Fields` , ' FROM ' , TableName , ' ' , SqlWhere , ' ',GroupFields , ') AS tempTb');
 	END IF;
 END IF;
 
-/*execute the `Sql` to generate the total record amount*/
-PREPARE stmt FROM @`Sql`;
-EXECUTE stmt USING @TotalRecordAmount;
+PREPARE stmt FROM @SqlCommand;
+EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
 
-END;
+SET TotalRecordAmount:=@TotalRecords;
+SELECT TotalRecordAmount;
+
+/*get the total page*/
+SET TotalPageAmount=CEILING((TotalRecordAmount+0.0)/PageSize);
+SELECT TotalPageAmount;
+
+
+END
